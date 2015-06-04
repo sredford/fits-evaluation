c7ef0937ad0924fd6f826b8527dcfb2d98c26fb4
Sophie Redford <sophie.redford@gmail.com>
2015-06-04 12:01:59 +0200

    Added code to get list of station ids.

--- station_data.py
+++ station_data.py
@@ -4,8 +4,21 @@ from urllib2 import urlopen
 from StringIO import StringIO
 from numpy import genfromtxt
 import ast
+import re
 
-station_ids = [ 2 ]
+stations_url_str = 'http://data.hisparc.nl/api/stations'
+stations_url = urlopen(stations_url_str)
+stations = stations_url.read()
+
+# can't just look for numbers as some names have numbers in
+# so look for "number: number"
+numbers = re.findall(r'"number": \d+', stations)
+station_ids = []
+for num in numbers:
+        station_ids.append(int(re.findall(r'\d+', num)[0]))
+
+print "Found these station ids"
+print station_ids
 
 base_url = "http://data.hisparc.nl/api/station/%d/"
 
